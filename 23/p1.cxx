440230a2273ce6bbf20124e473c473eab947f8ae
Benjamin Yates <benjamin@rqdq.com>
2018-12-22 21:20:35 -0600

    lib/template update

--- 23/p1.cxx
+++ 23/p1.cxx
@@ -15,30 +15,34 @@ using namespace std;
 
 #define sz(x) (int(x.size()))
 #define forn(i,n) for(int i=0;i<int(n);i++)
+#define watch(x) cout << (#x) << "(" << (x) << ")\n"
+#define dbg(x) (#x) << "(" << (x) << ") "
+#define fi first
+#define se second
 
+const string nl("\n");
+
+using ll = long long;
+using ii = pair<int, int>;
+using vi = vector<int>;
+using vii = vector<ii>;
+using vs = vector<string>;
+#define uset unordered_set
+#define umap unordered_map
+
+const int oo = 0x3f3f3f3f;
 
 struct ivec2 {
 	int x, y;
 
-	ivec2& operator+=(ivec2 rhs) {
-		x += rhs.x;
-		y += rhs.y;
-		return *this; } };
+	ivec2& operator+=(ivec2 b) { x += b.x; y += b.y; return *this; }
+	ivec2& operator-=(ivec2 b) { x -= b.x; y -= b.y; return *this; }
+	ivec2& operator*=(ivec2 b) { x *= b.x; y *= b.y; return *this; } };
 
 ivec2 vmin(ivec2 a, ivec2 b) { return ivec2{ min(a.x, b.x), min(a.y, b.y) }; }
 ivec2 vmax(ivec2 a, ivec2 b) { return ivec2{ max(a.x, b.x), max(a.y, b.y) }; }
 
 int area(ivec2 a) { return a.x * a.y; }
-
-const auto UP = ivec2{0,-1};
-const auto DOWN = ivec2{0,1};
-const auto LEFT = ivec2{-1,0};
-const auto RIGHT = ivec2{1,0};
-
-const auto NORTH = UP;
-const auto SOUTH = DOWN;
-const auto EAST = RIGHT;
-const auto WEST = LEFT;
 
 ostream& operator<<(ostream& stream, ivec2 item) {
 	stream << "(" << item.x << ", " << item.y << ")";
@@ -50,15 +54,27 @@ ivec2 operator*(ivec2 a, ivec2 b) { return ivec2{ a.x*b.x, a.y*b.y }; }
 
 bool operator==(ivec2 a, ivec2 b) { return (a.x==b.x && a.y==b.y); }
 
+const ivec2 UP{0,-1};
+const ivec2 DOWN{0,1};
+const ivec2 LEFT{-1,0};
+const ivec2 RIGHT{1,0};
+
+const auto NORTH = UP;
+const auto SOUTH = DOWN;
+const auto EAST = RIGHT;
+const auto WEST = LEFT;
+
 
 struct Rect {
-	ivec2 leftTop;
-	ivec2 rightBottom; };
+	ivec2 lt;
+	ivec2 rb;
+	ivec2 dim() const { return { rb.x-lt.x, rb.y-lt.y }; }
+	int area() const { return ::area(dim()); } };
 
 
 /* bfs
-	dequeue<pair<XXX, int>> queue;
-	unordered_set<XXX> visited;
+	deque<pair<XXX, int>> queue;
+	uset<XXX> visited;
 	queue.push_back({ xxx, 0 });
 	while (!queue.empty()) {
 		const auto[hpos, hdist] = queue.front();  queue.pop_front();
@@ -68,15 +84,15 @@ struct Rect {
 */
 
 /* dijkstra
-	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> queue;
-	unordered_set<int> visited;
-	unordered_map<int, int> dist;
+	priority_queue<ii, vii, greater<ii>> queue;
+	uset<int> visited;
+	umap<int, int> dist;
 
 	auto getDistance = [&](int key) {
 		if (auto search = dist.find(key); search != dist.end()) {
 			return search->second; }
 		else {
-			return INF; }};
+			return oo; }};
 
 	queue.push({ 0, INITIAL_POSITION });
 	dist[INITIAL_POSITION] = 0;
@@ -100,15 +116,15 @@ struct Rect {
 		// outLinks.push_back({ COST, NEXT_NODE });
 
 		for (const auto& link : outLinks) {
-			const auto& cost = link.first;
-			const auto& nextPos = link.second;
+			const auto& cost = link.fi;
+			const auto& nextPos = link.se;
 			int thisCost = getDistance(hpos) + cost;
 			if (getDistance(nextPos) > thisCost) {
 				dist[nextPos] = thisCost;
 				queue.push({ thisCost, nextPos }); }}}
 */
 
-ostream& operator<<(ostream& stream, vector<int> nums) {
+ostream& operator<<(ostream& stream, const vi& nums) {
 	bool first = true;
 	for (const auto& num : nums) {
 		if (first) {
@@ -120,54 +136,52 @@ ostream& operator<<(ostream& stream, vector<int> nums) {
 	return stream; }
 
 
-std::vector<std::string> split(const std::string& str, char ch) {
-	std::vector<std::string> items;
-	std::string src(str);
+vs split(const string& str, char ch) {
+	vs items;
+	string src(str);
 	auto nextmatch = src.find(ch);
 	while (1) {
-		auto item = src.substr(0, nextmatch);
-		items.push_back(item);
-		if (nextmatch == std::string::npos) { break; }
+		items.emplace_back(src.substr(0, nextmatch));
+		if (nextmatch == string::npos) { break; }
 		src = src.substr(nextmatch + 1);
 		nextmatch = src.find(ch); }
-
 	return items; }
 
 
 bool consumePrefix(string& text, const string& match) {
-	const int matchLen = match.size();
-	if (text.size() < matchLen) {
-		return false; }
-	if (text.substr(0, matchLen) == match) {
-		text = text.substr(matchLen);
+	if ((text.size() >= sz(match)) &&
+		(text.substr(0, sz(match)) == match)) {
+		text = text.substr(sz(match));
 		return true; }
 	return false; }
 
 
 bool consumeSuffix(string& text, const string& match) {
-	const int matchLen = match.size();
-	if (text.size() < matchLen) {
-		return false;}
-	if (text.substr(text.size()-matchLen, matchLen) == match) {
-		text = text.substr(0, text.size()-matchLen);
+	if ((text.size() >= sz(match)) &&
+	    (text.substr(sz(text)-sz(match), sz(match)) == match)) {
+		text = text.substr(0, sz(text)-sz(match));
 		return true; }
 	return false; }
 
 
 int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
 	/*
-	vecttor<string> lines;
+	vs lines;
 	string line;
 	while (getline(cin, line) {
 		lines.push_back(line); };
 	*/
 
 	/*
-	vector<int> nums;
+	vi nums;
 	int x;
 	while (cin >> x) {
 		nums.push_back(x); }
 	*/
+
 	int ax = 0;
-	cout << ax << "\n";
+	cout << ax << nl;
 	return 0; }
